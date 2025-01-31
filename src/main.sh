6ca6465519c6c4700bdc55ec68ce25a4222ccc8d
francesco <info.francescobianco@gmail.com>
2025-01-31 19:33:39 +0100

    Deploy

--- src/main.sh
+++ src/main.sh
@@ -1,10 +1,12 @@
 
 embed pluto
 embed alias_for_pluto pluto
+embed alias_for_main main
 embed manifest ../Manifest.toml
 
 main() {
   pluto
   alias_for_pluto
+  alias_for_main > main.txt
   manifest
 }
