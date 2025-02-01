1f753668011a41afe53ecba1ec27f0ed46397bd3
francesco <info.francescobianco@gmail.com>
2025-02-01 12:01:46 +0100

    Deploy

--- src/main.sh
+++ src/main.sh
@@ -3,17 +3,17 @@ extern package console
 
 embed pluto
 embed alias_for_pluto pluto
-embed alias_for_main main
-embed manifest ../Manifest.toml
+#embed alias_for_main main
+#embed manifest ../Manifest.toml
 
-embed console
-embed alias_for_console console
+#embed console
+#embed alias_for_console console
 
 main() {
   pluto
   alias_for_pluto
-  alias_for_main > main.txt
-  manifest
-  console
-  alias_for_console
+  #alias_for_main > main.txt
+  #manifest
+  #console
+  #alias_for_console
 }
